b39531a8d5647848ec509e881349a1d16753d04f
Bhattacharya <sumantra.bhattacharya@siemens.com>
2018-08-12 13:32:16 +0530

    Assignmnet_6.2_JavaScript basics

---
+++ script.js
@@ -0,0 +1,23 @@
+/*  Assigning values as per the assignmnet problem */
+var recipeName = 'Creamy squash linguine';
+var preparationTime = 45
+var recipeBy = 'Jimmy Cutter';
+var isVegetarian = true;
+var calories = 223.7;
+console.log("Recipe name: " + recipeName);
+console.log("Preparation time: " + preparationTime);
+console.log("Recipe by: " + recipeBy);
+console.log("Is Vegiterian Dish: " + isVegetarian);
+console.log("calories: " + calories);
+console.log("Recipe details after update");
+console.log("---------------------------");
+/* reassigning values to the same varibles */
+recipeBy = 'Jimmy Cutter George';
+preparationTime = 60;
+console.log("Recipe name: " + recipeName);
+console.log("Preparation time: " + preparationTime);
+console.log("Recipe by: " + recipeBy);
+console.log("Is Vegiterian Dish: " + isVegetarian);
+console.log("calories: " + calories);
+
+
